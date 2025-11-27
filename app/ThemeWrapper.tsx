b598a42bc517315b21ff89fcf3fa30e3871be526
ManishMandrai <manishkumarmandrai@gmail.com>
2025-11-27 22:37:46 +0530

    ok

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 
-import { useThemeSwitcher } from "@/components/theme-provider";
+// import { useThemeSwitcher } from "@/components/theme-provider";
 
-export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
-  const { theme } = useThemeSwitcher();
+// export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
+//   const { theme } = useThemeSwitcher();
 
-  return (
-    <div className={theme === "dark" ? "dark-theme" : "light-theme"}>
-      {children}
-    </div>
-  );
-}
+//   return (
+//     <div className={theme === "dark" ? "dark-theme" : "light-theme"}>
+//       {children}
+//     </div>
+//   );
+// }
